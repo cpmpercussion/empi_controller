69fc4223ae293a2180a83186843aed5e7bb5ac26
Charles Martin <cpm@charlesmartin.com.au>
2019-02-08 13:21:41 +0100

    updates

--- empi_controller.py
+++ empi_controller.py
@@ -1,2 +1,41 @@
 """EMPI Controller for Grove hardware """
 
+import math
+import time
+import sys
+from grove.i2c import Bus
+from grove.adc import ADC
+import RPi.GPIO as IO
+from numpy import interp
+
+class GroveRotaryAngleSensor(ADC):
+    def __init__(self, channel):
+        self.channel = channel
+        self.adc = ADC()
+
+    @property
+    def value(self):
+        return self.adc.read(self.channel)
+
+
+class GroveServo:
+    MIN_DEGREE = 0
+    MAX_DEGREE = 180
+    INIT_DUTY = 2.5
+
+    def __init__(self, channel):
+        IO.setup(channel,IO.OUT)
+        self.pwm = IO.PWM(channel,50)
+        self.pwm.start(GroveServo.INIT_DUTY)
+
+    def __del__(self):
+        self.pwm.stop()
+
+    def setAngle(self, angle):
+        # Map angle from range 0 ~ 180 to range 25 ~ 125
+        angle = max(min(angle, GroveServo.MAX_DEGREE), GroveServo.MIN_DEGREE)
+        tmp = interp(angle, [0, 180], [25, 125])
+        self.pwm.ChangeDutyCycle(round(tmp/10.0, 1))
+
+servo = GroveServo
+knob = GroveRotaryAngleSensor
--- grove_servo.py
+++ grove_servo.py
@@ -36,11 +36,11 @@ def main():
 
     while True:
         for x in range(0, 180):
-            print x, "degree"
+            print(x, "degree")
             servo.setAngle(x)
             time.sleep(0.05)
         for x in range(180, 0, -1):
-            print x, "degree"
+            print(x, "degree")
             servo.setAngle(x)
             time.sleep(0.05)
 
