1497b73145afab38d0d34e56f7db67506dc47e50
Charles Martin <cpm@charlesmartin.com.au>
2019-02-08 14:20:43 +0100

    display adjustments

--- empi_controller.py
+++ empi_controller.py
@@ -47,10 +47,22 @@ class GroveServo:
 servo = GroveServo
 knob = GroveRotaryAngleSensor
 
+def display_text(display, line1, line2=""):
+    # Set up image and draw some text.
+    display.clear()
+    width = display.width
+    height = display.height
+    image = Image.new('1', (width, height))
+    draw = ImageDraw.Draw(image)
+    draw.text((0, 0),    'Hello',  font=font, fill=255)
+    draw.text((0, 20), 'World!', font=font, fill=255)
+    # Display image.
+    display.image(image)
+    display.display()
+
 # Display init
-# Raspberry Pi pin configuration:
-RST = 24
 # 128x32 display with hardware I2C:
+RST = 24
 disp = Adafruit_SSD1306.SSD1306_128_32(rst=RST)  # , i2c_address=0x3C)
 # font = ImageFont.truetype('Minecraftia.ttf', 8)
 font = ImageFont.load_default()
@@ -59,13 +71,15 @@ disp.begin()
 disp.clear()
 disp.display()
 
-# Set up image and draw some text.
-width = disp.width
-height = disp.height
-image = Image.new('1', (width, height))
-draw = ImageDraw.Draw(image)
-draw.text((0, 0),    'Hello',  font=font, fill=255)
-draw.text((0, 20), 'World!', font=font, fill=255)
-# Display image.
-disp.image(image)
-disp.display()
+display_text(disp, "EMPI Booted and Loading MDRNN.")
+
+# # Set up image and draw some text.
+# width = disp.width
+# height = disp.height
+# image = Image.new('1', (width, height))
+# draw = ImageDraw.Draw(image)
+# draw.text((0, 0),    'Hello',  font=font, fill=255)
+# draw.text((0, 20), 'World!', font=font, fill=255)
+# # Display image.
+# disp.image(image)
+# disp.display()
